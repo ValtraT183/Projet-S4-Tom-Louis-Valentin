6c4962abb925905a3621cf3280ce4215fee3c62b
LouisC123456 <louis.chevreux@reseau.eseo.fr>
2026-03-19 08:56:29 +0100

    modif page connexion

--- Frame_connexion.py
+++ Frame_connexion.py
@@ -25,25 +25,25 @@ def creerFrameConnexion(parent,valider,quitter):
 
 
 
-    canva.create_text(400,50, text= "Connexion", justify=CENTER, fill = "white", font="Arial 50")
+    canva.create_text(200,100, text= "Connexion", justify=CENTER, fill = "firebrick", font="Rye 50")
 
-    canva.create_text(200,200, text= "Identifiant : ", justify=CENTER, fill = "white", font="Arial 30")
+    canva.create_text(200,300, text= "Identifiant : ", justify=CENTER, fill = "gold", font="Limelight 27")
     entree_id = Entry(parent, width= 50)
-    entree_id.place(x=650,y=200)
+    entree_id.place(x=550,y=300)
 
 
-    canva.create_text(200,250, text= "Mot de passe : ", justify=CENTER, fill = "white", font="Arial 30")
-    entree_mdp = Entry(parent)
-    entree_mdp.place(x=650,y=250)
+    canva.create_text(200,400, text= "Mot de passe : ", justify=CENTER, fill = "gold", font="Limelight 27")
+    entree_mdp = Entry(parent, width=50)
+    entree_mdp.place(x=550,y=400)
 
 
 
-    bouton_creer_compte = Button(parent,text="Créer un nouveau compte")        #CHANGER DE FRAME
-    bouton_valider = Button(parent,text="Se connecter", command=valider)
-    bouton_quit = Button(parent, text ="Quitter", command=quitter, bg='red')
-    bouton_creer_compte.place(x=500,y=500)
-    bouton_valider.place(x=200,y=600)
-    bouton_quit.place(x=700,y=600)  
+    bouton_creer_compte = Button(parent,text="Créer un nouveau compte",padx=20,pady=10)        #CHANGER DE FRAME
+    bouton_valider = Button(parent,text="Se connecter", command=valider,padx=20,pady=10)
+    bouton_quit = Button(parent, text ="Quitter", command=quitter, bg='red',padx=20,pady=10)
+    bouton_creer_compte.place(x=525,y=500)
+    bouton_valider.place(x=800,y=500)
+    bouton_quit.place(x=1250,y=600)  
 
     return {
         "frame": frame_connexion,
