a368a2fa4f6c076a77b9e4f3ce731bf48d524735
bechutom1-hash <bechu.tom1@gmail.com>
2026-04-02 23:01:42 +0200

    début newsletter

--- Casino/Frame_menu.py
+++ Casino/Frame_menu.py
@@ -1,9 +1,51 @@
 from tkinter import *
 from PIL import Image, ImageTk 
+from random import*
+
+
 
 def creerFrameMenu(parent, retourner, nom_uti, solde_uti, quitter, creerMachineASous, creerJeuDeDes, creerBlackjack, creerRoulette):
+    
+    
+   
+    def affichageNewsletter(liste):
+        canva.delete("newsletter")
+        info = randint(0,len(liste)-1)
+        canva.create_text(65,350,text=liste[info],font=("Arial",15),fill="white",tags="newsletter",anchor=W)
+        canva.after(10000,lambda :affichageNewsletter(liste))
 
 
+    
+
+    def indiceSoldeLePlusHaut():
+        with open("Compte.txt","r",encoding="utf8") as compte :
+            lignes = compte.readlines()
+            soldes = []
+            for ligne in lignes :
+                liste = ligne.strip().split("/")
+                soldes.append(int(liste[2])) 
+            haut = soldes[0]
+            indice=0
+            for i in range(len(soldes)):
+                if soldes[i] > haut :
+                    indice = i
+                    haut = soldes[i]
+
+            return indice
+                    
+                
+
+    def nomSoldeUtiStar(indice) :
+        with open("Compte.txt","r",encoding="utf8") as compte :
+            lignes = compte.readlines()
+            info_joueur = lignes[indice].strip().split("/")
+            return (info_joueur[0],info_joueur[2])
+        
+
+    
+
+   
+    
     # Création de la frame
     frame_menu = Frame(parent, width=1500, height=750)
 
@@ -48,8 +90,26 @@ def creerFrameMenu(parent, retourner, nom_uti, solde_uti, quitter, creerMachineA
     # Affichage du nom de l'utilisateur et du solde
 
     canva.create_text(1350,50,text=f"Nom d'utilisateur : {nom_uti}",font=("Arial",15),fill="white")
-
     canva.create_text(1350,100,text=f"Solde : {solde_uti} VTL",font=("Arial",15),fill="white")
+
+
+    # Affichage de la newsletter
+    nom,solde= nomSoldeUtiStar(indiceSoldeLePlusHaut())
+    newsletter = [f"Utilisateur stars : {nom}, {solde} VTL"]
+    
+    canva.create_text(225,150,text="Newsletter",fill='gold',font="Rye 37")
+    
+    canva.create_line(50,100,400,100,width=4,fill = "gold")
+    canva.create_line(50,675,400,675,width=4,fill = "gold")
+    
+    canva.create_line(50,100,50,675,width=4,fill = "gold")
+    canva.create_line(400,100,400,675,width=4,fill = "gold")
+
+    affichageNewsletter(newsletter)
+
+
+
+
 
     return{
         "frame": frame_menu,
