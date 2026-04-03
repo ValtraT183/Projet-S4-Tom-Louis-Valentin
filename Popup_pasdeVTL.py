394716590534c219d0b178d37e1e94afec2e1c3d
ValtraT183 <valentin.daguin@reseau.eseo.fr>
2026-04-03 11:35:01 +0200

    decompte popup

--- Popup_pasdeVTL.py
+++ Popup_pasdeVTL.py
@@ -5,11 +5,22 @@ def afficherPopUpPasDeVTL(parent):
 
     fen = Toplevel(parent)
 
-    Label(fen, text="Vous n'avez plus de VTL, votre compte est supprimé").pack()
+    canva = Canvas(fen, height=300, width=500)
+    canva.pack()
 
+    canva.create_text(150,50, text="Vous n'avez plus de VTL, votre compte est supprimé")
 
-    def retour():
+    def decompte(i):
+        i=i-1
+        canva.delete("temps")
+        if i>0:
+            if i>1:
+                canva.create_text(250,150,text=f"Vous serez redirigé vers la page de connexion dans {i} secondes", tags="temps")
+            else:
+                canva.create_text(250,150,text=f"Vous serez redirigé vers la page de connexion dans {i} seconde", tags="temps")
+            canva.after(1000, lambda : decompte(i))
 
-        fen.destroy()
+        else:
+            fen.destroy()
 
-    Button(fen, text="Retour", command=retour).pack()
+    decompte(5)
